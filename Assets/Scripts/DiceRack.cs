5811a0aa28073462b5c0228b13fe60fcf116162b
Nicolas B <nicolas.brouet84@gmail.com>
2022-07-15 22:43:59 +0200

    move scripts

--- Assets/Scripts/DiceRack.cs
+++ Assets/Scripts/DiceRack.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DiceRack : MonoBehaviour
 {
+    private List<Dice> diceRack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        diceRack = new DiceRack();    
     }
 
     // Update is called once per frame
